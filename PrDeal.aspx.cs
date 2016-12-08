ccd7893053a7032405dd9c68f21d37d6eb5ac6ad
Cruggeri <Keirwryn@gmail.com>
2016-12-08 13:20:14 -0600

    Add files via upload

--- PrDeal.aspx.cs
+++ PrDeal.aspx.cs
@@ -9,6 +9,16 @@ public partial class PrDeal : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (Session["user"] == null)
+        {
+            Response.Write("You must be logged in to view the Members area - Please sign in");
+            Response.Write("<a href='Loginpage.aspx'> sign in</a>");
+        }
+        else
+        {
+            Response.Write("<h3>Exclusive deals for you, " + Session["user"].ToString() + " </h3>");
+            Response.Write("<br/><a href='logout.aspx'>Logout</a>");
 
+        }
     }
 }
